5f9cd6b73d116e37bb4d847e506c9e4d7cba723d
Tássyla Lima <56099401+tatitassyla@users.noreply.github.com>
2021-07-13 23:31:21 -0300

    As Meias de Rangel

---
+++ 2839.py
@@ -0,0 +1,10 @@
+# Alunas: Izabela A. Andrade (20192004795), Marcela P. Silvério (20192020028) e Tássyla L. Lima (20192001990)
+# Turma: INF3A
+
+# **************************************************************************************************
+# Descrição do código: Para termos certeza que Rangel pegará pelo menos um par de meias, temos que
+# imaginar que na pior hipótese ele pegará uma meia de cada par, assim ao pegar a próxima ele 
+# com certeza formará pelo menos um par.
+# **************************************************************************************************
+
+print(int(input()) + 1)
