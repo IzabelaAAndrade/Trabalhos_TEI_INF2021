a2dc298fd5ab35b0c65d82c895de543726a2c597
Marcela Prata Silvério <marcelapsilverio@gmail.com>
2021-07-17 11:30:05 -0300

    Basquete de Robôs

--- 12_2780.py
+++ 12_2780.py
@@ -1,3 +1,12 @@
+# Alunas: Izabela A. Andrade (20192004795), Marcela P. Silvério (20192020028) e Tássyla L. Lima (20192001990)
+# Turma: INF3A
+
+# **************************************************************************************************
+# Descrição do Código: O código recebe o valor da distância correspondente a posição de um robô em
+# relação ao início de uma quadra de basquete. Dependendo dessa variável, nós imprimimos a quantidade
+# de pontos que uma cesta feita nessa distância corresponde.
+# **************************************************************************************************
+
 
 distancia = int(input())
 
